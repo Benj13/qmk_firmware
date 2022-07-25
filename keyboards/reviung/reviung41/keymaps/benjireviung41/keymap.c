e71ee04eebc193adbf6d6f74e1ed713ac3be47f2
Benji <benjamindumas@hotmail.fr>
2022-07-25 21:35:58 +0200

    Small adjustments

--- keyboards/reviung/reviung41/keymaps/benjireviung41/keymap.c
+++ keyboards/reviung/reviung41/keymaps/benjireviung41/keymap.c
@@ -40,31 +40,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
   * | Ctrl |   Z  |   X  |   C  |   V  |   B  |         |   N  |   M  |   ,  |   .  |   /  | Enter|
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
-  *                             | Lower|  GUI |  Space  |  Alt | Raise|
+  *                             |  GUI | Lower|  Space  | Raise|  Alt |
   *                             +-------------/         \-------------+
   */
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_QUOT,
-    KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  RSFT_T(KC_BSPC),
+    KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_BSPC,
     KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
-                                            LOWER,  KC_RGUI,    KC_SPC, KC_LALT,     RAISE
+                                            KC_RGUI,   LOWER,   KC_SPC,    RAISE,    KC_LALT
   ),
 
   /* Lower Layer - Symbols
   * ,-----------------------------------------+         +-----------------------------------------.
   * |  Esc |   !  |   @  |   #  |   $  |   %  |         |   ^  |   &  |   *  |   (  |   )  |  Del |
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
-  * |      |   -  |   =  |   [  |   ]  |   |  |         | Left | Down |  Up  | Right|   `  |   ~  |
+  * |      |   -  |   =  |   [  |   ]  |   |  |         | Left | Down |  Up  | Right|   `  | Bksp |
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
-  * |      |      |   ç  |   Å  |   Ä  |   Ö  |         | Home |  End | PgUp | PgDn |      |      |
+  * |      |      |   ç  |   Å  |   Ä  |   Ö  |         | Home |  End | PgUp | PgDn |   ~  | Enter|
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
   *                             |      |      |  Space  |      |      |
   *                             +-------------/         \-------------+
   */
   [_LOWER] = LAYOUT_reviung41(
     KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
-    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
-    _______,  FR_CCED,  SE_ARNG,  SE_ADIA,  SE_ODIA,   _______,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  _______,  _______,
+    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_BSPC,
+    _______,  FR_CCED,  SE_ARNG,  SE_ADIA,  SE_ODIA,   _______,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_TILD,  KC_ENT,
                                             _______,   _______,  KC_SPC,   _______,  _______
   ),
 
@@ -74,15 +74,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |         |  F7  |  F8  |  F9  |  F10 | F11  | F12  |
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
-  * |      |      |      |      |      |      |         |      |      |      |      |      |      |
+  * |      |      |      |      |      |      |         |      |      |      |      |      | Enter|
   * |------+------+------+------+------+------+         +------+------+------+------+------+------|
-  *                             | Raise|  GUI |  Space  |  Alt | Lower|
+  *                             |      |      |  Space  |      |      |
   *                             +-------------/         \-------------+
   */  
   [_RAISE] = LAYOUT_reviung41(
      KC_ESC,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
       KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-    _______,  _______,  _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______,  _______,  KC_ENT,
                                             _______,   _______,  KC_SPC,   _______,  _______
   ),
   
