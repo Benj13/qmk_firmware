fb7d2af44694432a3ac432195aea4e086dff7c60
Benji <benjamindumas@hotmail.fr>
2022-11-23 19:10:39 +0100

    Fix Hubris keymap

--- keyboards/thevankeyboards/minivan/keymaps/benjihubris/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/benjihubris/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,  KC_R, KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_DEL,
     KC_TAB,  KC_A,    KC_S,    KC_D,  KC_F, KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V, KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, KC_LGUI, LOWER,   ADJUST,                KC_SPC,          RAISE, KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_LCTL, KC_LGUI, LOWER,   ADJUST,               KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   /* Symbols
   * ,--------------------------------------------------------------------------.
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `--------------------------------------------------------------------------'
   */
   [_LOWER] = LAYOUT_arrow( 
-    KC_GQUOT KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,  KC_EQUAL, KC_PLUS, 
+    KC_GRV,  KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,  KC_EQUAL, KC_PLUS, 
     KC_BSLS, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_SCLN, KC_COLN, KC_PIPE,  KC_RPRN,  KC_RBRC, KC_RCBR, _______,
     _______, FR_CCED, SE_ARNG, SE_ODIA, SE_ADIA, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______,          _______,                    _______,  _______, _______, _______
@@ -76,13 +76,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
-    _______, _______, _______, _______,          _______,                   _______, KC_HOME, KC_PGDN,  KC_END
+    _______, _______, KC_LALT, _______,          _______,                   _______, KC_HOME, KC_PGDN,  KC_END
   ),
   [_ADJUST] = LAYOUT_arrow( 
-    QK_BOOT, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
-    _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______,  KC_F11,  KC_F12, _______, _______,
+    QK_BOOT, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,   KC_F5,   KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  _______,
+    _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, KC_F11,  KC_F12,  _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______,          _______,                    _______, _______, _______, _______
+    _______, _______, _______, _______,          _______,                   _______, _______, _______, _______
   )
 };
 
