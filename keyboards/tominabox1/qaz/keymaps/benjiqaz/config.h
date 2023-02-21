8eba5811b3a3d26f278e4165ee50cfe11daa8c95
Benji <benjamindumas@hotmail.fr>
2023-02-21 19:51:43 +0100

    Update fork and update qaz

--- keyboards/tominabox1/qaz/keymaps/benjiqaz/config.h
+++ keyboards/tominabox1/qaz/keymaps/benjiqaz/config.h
@@ -4,3 +4,7 @@
 #    define COMBO_COUNT 8
 #    define COMBO_TERM 30
 #endif
+
+#define IGNORE_MOD_TAP_INTERRUPT //helps with homerow mods
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD_PER_KEY
